e1d94222cac222560dbc40a8b80eb904f5d01cea
CoderRokade <vishalrokade77104@gmail.com>
2024-05-25 07:38:51 +0530

    first commit

--- Backend/src/db/connection.ts
+++ Backend/src/db/connection.ts
@@ -1,15 +1,19 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import { connect,disconnect } from 'mongoose';
 import mongoose,{ConnectOptions} from 'mongoose';
-import * as dotenv from 'dotenv';
-dotenv.config({ path: 'env' });
+
+
+dotenv.config({ path: '../env' }).parsed;
 
 
 async function connectToDatabase() {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI,{
-      useNewUrlParser: true, 
-      useUnifiedTopology: true,
+    const conn = await mongoose.connect(process.env.MONGODB_URL
+      // ,{useNewUrlParser: true, 
+      // useUnifiedTopology: true,
      
-  })    //mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  )    //mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   } catch (error) {
     console.log(error);
     throw new Error("Could not Connect To MongoDB");
